6a26216dd0a87e8652e6c84a85f9ab6ba6f9cae0
skewnart <corentinz@outlook.fr>
2026-04-13 13:25:42 +0200

    feat(module 2 - partie 1): Console part

---
+++ Module 2/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // # Partie 1 : Console 
+
+            Console.Write("Quel est votre nom : ");
+            Console.ReadLine();
+
+            Console.Write("Quel est votre âge : ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nAppuyez sur une touche pour terminer le programme...");
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## Correction des exercices
 
 - [Module 1 - Introduction au Framework](https://github.com/Skewnart/Supinfo-2026-3ASP/tree/main/Module%201)
+- [Module 2 - Bases du C# partie 1](https://github.com/Skewnart/Supinfo-2026-3ASP/tree/main/Module%202)
